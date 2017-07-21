7cc884f2b871545dff30d2da649253892c0a844d
Francesca Giordano <fgiordano.data@gmail.com>
2017-07-21 15:31:52 +0100

    changed gzstream lniking

--- utils/install.sh
+++ utils/install.sh
@@ -5,8 +5,6 @@ myforACT=$1
 thisdir=`pwd`
 
 cd $myforACT/utils/mysrcs
-echo
-
 mkdir -p mylibs
 
 ### Intalling gzstream (it needs zlib!)
@@ -25,11 +23,11 @@ if [[ ! -d  gzstream ]]  || [[ ! -f gzstram/gzstream.o ]]; then
 	tar -xvzf gzstream.tgz &> /dev/null
 	if [[ "$?" != 0 ]]; then echo " Error during gzstream un-compressing. Exiting now"; exit; fi
 	cd gzstream
-	make ##&> /dev/null
-	echo $?
+	make &> /dev/null
+	
 	if [[ "$?" != 0 ]]; then echo " Error during gzstream compilation. Exiting now"; exit; fi
 	test=`make test | grep "O.K" | wc -l`
-	echo $test
+
 	if [[ $test == 1 ]]; then echo " "1. gzstream installed; rm ../gzstream.tgz 
 	else  echo  " Gzstream test failed. Exiting now"; exit; fi
     fi
@@ -41,8 +39,10 @@ if [[ ! -f gzstream.o ]]; then
 fi
 
 
-OLD_CPLUS_INCLUDE_PATH=`echo $CPLUS_INCLUDE_PATH`
-export CPLUS_INCLUDE_PATH=$myforACT/utils/mysrcs/mylibs/gzstream:${CPLUS_INCLUDE_PATH}
+#OLD_CPLUS_INCLUDE_PATH=`echo $CPLUS_INCLUDE_PATH`
+#export CPLUS_INCLUDE_PATH=$myforACT/utils/mysrcs/mylibs/gzstream:${CPLUS_INCLUDE_PATH}
+
+
 
 cd $myforACT/utils/mysrcs/
 
@@ -58,4 +58,4 @@ done
 
 
 ## I guess I need to define the gzstream path only during compilation?
-export CPLUS_INCLUDE_PATH=$OLD_CPLUS_INCLUDE_PATH
+#export CPLUS_INCLUDE_PATH=$OLD_CPLUS_INCLUDE_PATH
